dbdbb0f78c38404058c40dd957e7168aef6e76cc
Alexander Frankel <alexxander.frankel@gmail.com>
2016-03-30 17:32:18 -0400

    Remove contents of tmp dir after build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rubyxls'
+
+RSpec.configure do |config|
+
+  config.after(:all) do
+    FileUtils.rm_rf("#{tmp_dir_path}/.")
+  end
+
+end
+
+def tmp_dir_path
+  File.expand_path('../../tmp/', __FILE__)
+end
