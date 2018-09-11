67c2e70a09f9bd16456c2b2046b10149d8c70903
Charles McMillan <charlesmcm@viewthespace.com>
2018-09-11 16:07:36 -0400

    Bump version

--- spec/rubyxls_spec.rb
+++ spec/rubyxls_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe Rubyxls do
-
-  it 'has correct version number' do
-    expect(Rubyxls::VERSION).to eq("1.0.4")
-  end
-
   describe '#generate_default_report' do
     subject { Rubyxls.generate_default_report }
 
